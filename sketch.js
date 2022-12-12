53cf39a0f629308cd1b071de4c760f974deab305
Thomas Lynn <thomas2.lynn@live.uwe.ac.uk>
2022-12-12 11:12:28 +0000

    abba

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let xSpeed, ySpeed, xDir, yDir;
 let shapeArr = [];
 
 function windowResized() {
-    if (windowWidth<600) { 
-    resizeCanvas(windowWidth-20,windowHeight);
-  }
+  if (windowWidth<600) { 
+  resizeCanvas(windowWidth-20,windowHeight);
+}
 }
 
 function setup() {
--- sketch1.js
+++ sketch1.js
@@ -2,27 +2,27 @@ var canvas1;
 let circRotateClock = 0;
 
 function windowResized() {
-  if (windowWidth<600) { 
-  resizeCanvas(windowWidth-20,windowHeight);
-}
+  if (windowWidth < 600) {
+    resizeCanvas(windowWidth - 20, windowHeight);
+  }
 }
 
 function setup() {
   canvas1 = createCanvas(600, 600);
   canvas1.parent("canvasContainer1");
-  background(color(30,41,82));
+  background(color(30, 41, 82));
   angleMode(DEGREES);
 }
 
 function draw() {
   background(30, 41, 82, 70);
   translate(width / 2, height / 2);
-  rotate(circRotateClock*1.45);
+  rotate(circRotateClock * 1.45);
   circRotateClock++;
   noFill();
   stroke(139, 0, 0);
   strokeWeight(2);
-  
+
   for (let i = 0; i < 100; i++) {
     console.log(i);
 
@@ -31,7 +31,7 @@ function draw() {
     arc(75 + 32, 0, 40, 27.5, 180, 0);
     arc(75 + 32 * 2, 0, 25, 20, 0, 180);
     arc(75 + 32 * 3, 0, 40, 27.5, 180, 0);
-    ellipse(0,0,381,381);
-    ellipse(0,0,125,125);
+    ellipse(0, 0, 381, 381);
+    ellipse(0, 0, 125, 125);
   }
 }
