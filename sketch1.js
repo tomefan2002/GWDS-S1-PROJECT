fc5709c03eec199539984ef8717b2cbfdd8f6dec
Thomas Lynn <thomas2.lynn@live.uwe.ac.uk>
2022-12-12 10:21:33 +0000

    Create sketch1.js

---
+++ sketch1.js
@@ -0,0 +1,29 @@
+let circRotateClock = 0;
+
+function setup() {
+  createCanvas(600, 600);
+  background(color(30,41,82));
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(30, 41, 82, 70);
+  translate(width / 2, height / 2);
+  rotate(circRotateClock*1.45);
+  circRotateClock++;
+  noFill();
+  stroke(139, 0, 0);
+  strokeWeight(2);
+  
+  for (let i = 0; i < 100; i++) {
+    console.log(i);
+
+    rotate(3.6);
+    arc(75, 0, 25, 20, 0, 180);
+    arc(75 + 32, 0, 40, 27.5, 180, 0);
+    arc(75 + 32 * 2, 0, 25, 20, 0, 180);
+    arc(75 + 32 * 3, 0, 40, 27.5, 180, 0);
+    ellipse(0,0,381,381);
+    ellipse(0,0,125,125);
+  }
+}
